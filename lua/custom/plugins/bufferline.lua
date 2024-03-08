627586d5ec67bf7b635d33b4f7427762492233d8
Jacob <turkeybird98@icloud.com>
2024-03-08 11:12:23 -0600

    added setup for bufferline

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -2,4 +2,8 @@ return {
   'akinsho/bufferline.nvim',
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    vim.opt.termguicolors = true
+    require('bufferline').setup {}
+  end,
 }
