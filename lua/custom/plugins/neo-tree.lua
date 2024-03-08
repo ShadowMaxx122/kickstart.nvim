b8e926447bbad51ed949c288580734358cc1eb96
Jacob <turkeybird98@icloud.com>
2024-03-08 11:23:21 -0600

    neo-tree closes on file opening

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -14,6 +14,19 @@ return {
           hide_dotfiles = false,
           hide_gitignored = true,
         },
+
+        event_handlers = {
+
+          {
+            event = 'file_opened',
+            handler = function(file_path)
+              -- auto close
+              -- vimc.cmd("Neotree close")
+              -- OR
+              require('neo-tree.command').execute { action = 'close' }
+            end,
+          },
+        },
       },
       vim.keymap.set('n', '<leader>ff', ':Neotree filesystem reveal left<CR>', {}),
     }
